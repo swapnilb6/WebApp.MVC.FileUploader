ea33d32589bee9198cef7a966aebcbca29db26bf
SWAPNIL BAWANKAR <swapnilb6@gmail.com>
2022-08-01 15:36:23 +0530

    Add project files.

---
+++ WebApp.MVC.FileUploader/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using WebApp.MVC.FileUploader.Models;
+
+namespace WebApp.MVC.FileUploader.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult FileUpload()
+        {
+            ViewBag.ActionMsg = "null";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FileUpload(Employee emp)
+        {
+            string path = null;
+            string filename =null;
+            string fullPath = null;
+            ViewBag.ActionMsg = "null";
+            try
+            {
+                if (emp.file != null)
+                {
+                    path = Server.MapPath("~/App_Data/Uploaded Files");
+                    filename = Path.GetFileName(emp.file.FileName);
+                    fullPath = Path.Combine(path, filename);
+
+                    emp.file.SaveAs(fullPath);
+
+                    ViewBag.ActionMsg = "File Uploaded Successfully";
+                }
+                else
+                {
+                    ViewBag.ActionMsg = "Please Select a file.";
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return View();
+        }
+    }
+}
---
+++ WebApp.MVC.FileUploader/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.MVC.FileUploader.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        public HttpPostedFileBase file { get; set; }
+
+    }
+}
